1b6634aea3cbf84cf495fcc670be6b0ad6a2de40
Andrew Maddox <andrew.iain.maddox@gmail.com>
2023-08-28 16:49:22 +0100

    Adding buttons to the register page

--- src/Register.tsx
+++ src/Register.tsx
@@ -6,34 +6,58 @@ import { ReusableModal } from './components/ReusableModal'; // Import ReusableMo
 export const Register = () => {
   const navigate = useNavigate();
 
-  // State variables for modal and party name
-  const [isOpen, setIsOpen] = useState(false);
-  const [partyName, setPartyName] = useState('');
+  // State variables for modals and party names
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [partyName1, setPartyName1] = useState('');
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [partyName2, setPartyName2] = useState('');
 
-  // Function to handle party name submission
-  const handlePartyNameSubmit = () => {
-    // Your logic to handle the party name, maybe save it to database
-    setIsOpen(false); // Close the modal
-    navigate('/main'); // Navigate to main page
+  // Functions to handle party name submissions
+  const handlePartyNameSubmit1 = () => {
+    // Your logic to handle the first party name
+    setIsOpen1(false);
+    navigate('/main');
+  };
+
+  const handlePartyNameSubmit2 = () => {
+    // Your logic to handle the second party name
+    setIsOpen2(false);
+    navigate('/main');
   };
 
   return (
     <Box p={8}>
-
-      <Button mt={4} onClick={() => setIsOpen(true)}>
-        Open Modal to Enter Party Name
+      <Button mt={4} onClick={() => setIsOpen1(true)}>
+        Open First Modal to Enter Party Name
       </Button>
 
-      {/* Use ReusableModal here */}
+      <Button mt={4} ml={4} onClick={() => setIsOpen2(true)}>
+        Open Second Modal to Enter Party Name
+      </Button>
+
+      {/* Use ReusableModal for the first button */}
       <ReusableModal
-        isOpen={isOpen}
-        onClose={() => setIsOpen(false)}
-        title="Enter Party Name"
-        handleSubmit={handlePartyNameSubmit}
+        isOpen={isOpen1}
+        onClose={() => setIsOpen1(false)}
+        title="Enter First Party Name"
+        handleSubmit={handlePartyNameSubmit1}
       >
         <FormControl>
           <FormLabel>Party Name</FormLabel>
-          <Input value={partyName} onChange={(e) => setPartyName(e.target.value)} />
+          <Input value={partyName1} onChange={(e) => setPartyName1(e.target.value)} />
+        </FormControl>
+      </ReusableModal>
+
+      {/* Use ReusableModal for the second button */}
+      <ReusableModal
+        isOpen={isOpen2}
+        onClose={() => setIsOpen2(false)}
+        title="Enter Second Party Name"
+        handleSubmit={handlePartyNameSubmit2}
+      >
+        <FormControl>
+          <FormLabel>Party Name</FormLabel>
+          <Input value={partyName2} onChange={(e) => setPartyName2(e.target.value)} />
         </FormControl>
       </ReusableModal>
     </Box>
