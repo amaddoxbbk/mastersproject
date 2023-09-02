20ad360906e0030291be40111f70bc19c16db1ee
Andrew Maddox <andrew.iain.maddox@gmail.com>
2023-09-02 19:29:33 +0100

    Populate list of guests for event with call to db

---
+++ api/getGuests.ts
@@ -0,0 +1,27 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+import pkg from 'pg';
+const { Pool } = pkg;
+
+const pool = new Pool({
+  connectionString: "postgres://default:jaOhQqS02gyZ@ep-soft-bread-41762341-pooler.us-east-1.postgres.vercel-storage.com:5432/verceldb",
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+export default async (req: VercelRequest, res: VercelResponse) => {
+  try {
+    const { event_id: eventId } = req.body; // Get the event_id from the request body
+
+    if (!eventId) {
+      res.status(400).json({ error: 'Missing event_id parameter' });
+      return;
+    }
+
+    const result = await pool.query('SELECT * FROM attendees WHERE event_id = $1', [eventId]);
+    res.status(200).json(result.rows);
+  } catch (error) {
+    console.error("Error fetching guests:", error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,14 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { List, ListItem, Box } from "@chakra-ui/react";
 import { AddGuestButton } from "./components/AddGuestButton";
 import { useEvent } from "./components/EventContext";
+import axios from "axios";
 
 export const MainPage = () => {
   const { eventData } = useEvent();
-  const [list, setList] = useState<string[]>([]);
+  const [guests, setGuests] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(false); // Loading state
 
-  const addGuestToList = (name: string) => {
-    setList([...list, name]);
+  // Function to fetch guests from the database
+  const fetchGuests = async () => {
+    setIsLoading(true); // Set loading state to true before fetching
+    try {
+      const response = await axios.post("/api/getGuests", {
+        event_id: eventData.event_id,
+      });
+      setGuests(response.data);
+    } catch (error) {
+      console.error("Error fetching guests:", error);
+    }
+    setIsLoading(false); // Set loading state to false after fetching
+  };
+
+  useEffect(() => {
+    // Fetch guests when the component mounts or eventData.event_id changes
+    if (eventData.event_id) {
+      fetchGuests();
+    }
+  }, [eventData.event_id]);
+
+  const addGuestToList = async (name: string) => {
+    // refetch the list of guests to update the UI
+    await fetchGuests();
   };
 
   return (
@@ -17,9 +41,15 @@ export const MainPage = () => {
       <h2>Event ID: {eventData.event_id}</h2>
       <AddGuestButton addGuestToList={addGuestToList} />
       <List mt={4}>
-        {list.map((item, index) => (
-          <ListItem key={index}>{item}</ListItem>
-        ))}
+        {isLoading ? (
+          <ListItem>Loading...</ListItem>
+        ) : guests.length > 0 ? (
+          guests.map((guest, index) => (
+            <ListItem key={index}>{guest.attendee_name}</ListItem>
+          ))
+        ) : (
+          <ListItem>No guests added yet</ListItem>
+        )}
       </List>
     </Box>
   );
