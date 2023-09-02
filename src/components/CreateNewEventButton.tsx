582fe305b9ffb5cf6a4c636c43e62942f7ded58f
Andrew Maddox <andrew.iain.maddox@gmail.com>
2023-09-02 11:20:36 +0100

    Added validation to creating a new event

--- src/components/CreateNewEventButton.tsx
+++ src/components/CreateNewEventButton.tsx
@@ -1,6 +1,35 @@
 import React, { useState } from 'react';
 import { Button, FormControl, FormLabel, Input } from '@chakra-ui/react';
 import { ReusableModal } from './ReusableModal';
+import { z } from 'zod';
+import { Stack } from '@chakra-ui/react';
+
+// Define Zod schema
+const newEventSchema = z.object({
+  eventName: z.string().min(3, "Full event name is required"),
+  eventDate: z.string()
+    .min(1, "Event date is required")
+    .refine(date => {
+      const today = new Date();
+      const [day, month, year] = date.split("/");
+      const eventDate = new Date(`${year}-${month}-${day}`);
+      return eventDate > today;
+    }, "Event date must be in the future"),
+  eventLocation: z.string().min(1, "Event location is required").max(100, "Event location name is too long"),
+  numTopTables: z.number().min(0, "Negative number inputs not valid").max(1, "You cannot have more than one top table"),
+  maxSizeTopTable: z.number()
+    .min(1, "Max size of top table must be greater than zero"),
+  numNormalTables: z.number().min(0, "Negative number inputs not valid"),
+  maxSizeNormalTable: z.number()
+    .min(1, "Max size of normal table must be greater than zero"),
+})
+.refine(data => {
+  if (data.numTopTables === 0) {
+    return data.maxSizeTopTable === 0;
+  }
+  return true;
+}, { message: "You have entered a top table size with no top table. Please set table size to zero or add a top table", path: ['numTopTables', 'maxSizeTopTable'] });
+
 
 interface CreateNewEventButtonProps {
   handleNewEventSubmit: (
@@ -12,7 +41,7 @@ interface CreateNewEventButtonProps {
     numNormalTables: number,
     maxSizeNormalTable: number
   ) => void;
-  eventOptions: { value: string; label: string }[]; // Additional prop for the list of existing events
+  eventOptions: { value: string; label: string }[];
 }
 
 export const CreateNewEventButton: React.FC<CreateNewEventButtonProps> = ({ handleNewEventSubmit, eventOptions }) => {
@@ -24,58 +53,78 @@ export const CreateNewEventButton: React.FC<CreateNewEventButtonProps> = ({ hand
   const [maxSizeTopTable, setMaxSizeTopTable] = useState(0);
   const [numNormalTables, setNumNormalTables] = useState(0);
   const [maxSizeNormalTable, setMaxSizeNormalTable] = useState(0);
-  const [errorMessage, setErrorMessage] = useState("");
+  const [fieldErrors, setFieldErrors] = useState<{ [key: string]: string }>({});
 
   const handleSubmit = () => {
-    if (!eventName || !eventDate || !eventLocation || numTopTables < 0 || maxSizeTopTable < 0 || numNormalTables < 0 || maxSizeNormalTable < 0) {
-      setErrorMessage("All fields must be completed.");
+    const parsedData = newEventSchema.safeParse({
+      eventName,
+      eventDate,
+      eventLocation,
+      numTopTables,
+      maxSizeTopTable,
+      numNormalTables,
+      maxSizeNormalTable,
+    });
+
+    if (!parsedData.success) {
+      const errors: { [key: string]: string } = {};
+      parsedData.error.issues.forEach((issue) => {
+        errors[issue.path[0]] = issue.message;
+      });
+      setFieldErrors(errors);
       return;
     }
-  
+
     if (eventOptions.some(option => option.label === eventName)) {
-      setErrorMessage("An event with this name already exists.");
+      setFieldErrors({ eventName: "An event with this name already exists." });
       return;
     }
-  
+
     handleNewEventSubmit(eventName, eventDate, eventLocation, numTopTables, maxSizeTopTable, numNormalTables, maxSizeNormalTable);
     setIsOpenNew(false);
-    setErrorMessage(""); // Clear any existing error messages
+    setFieldErrors({});
   };
-  
 
-  const newEventForm = (
-    <>
-      {errorMessage && <div style={{ color: "red" }}>{errorMessage}</div>}
-      <FormControl>
-        <FormLabel>Event Name</FormLabel>
-        <Input value={eventName} onChange={(e) => setEventName(e.target.value)} />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Event Date</FormLabel>
-        <Input type="date" value={eventDate} onChange={(e) => setEventDate(e.target.value)} />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Event Location</FormLabel>
-        <Input value={eventLocation} onChange={(e) => setEventLocation(e.target.value)} />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Number of Top Tables</FormLabel>
-        <Input type="number" value={numTopTables} onChange={(e) => setNumTopTables(parseInt(e.target.value))} />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Max Size of Top Table</FormLabel>
-        <Input type="number" value={maxSizeTopTable} onChange={(e) => setMaxSizeTopTable(parseInt(e.target.value))} />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Number of Normal Tables</FormLabel>
-        <Input type="number" value={numNormalTables} onChange={(e) => setNumNormalTables(parseInt(e.target.value))} />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Max Size of Normal Table</FormLabel>
-        <Input type="number" value={maxSizeNormalTable} onChange={(e) => setMaxSizeNormalTable(parseInt(e.target.value))} />
-      </FormControl>
-    </>
-  );
+const newEventForm = (
+  <Stack spacing={6}>
+    <FormControl>
+      {fieldErrors.eventName && <div style={{ color: "red" }}>{fieldErrors.eventName}</div>}
+      <FormLabel mb={1}>Event Name</FormLabel>
+      <Input value={eventName} onChange={(e) => setEventName(e.target.value)} />
+    </FormControl>
+    <FormControl>
+      {fieldErrors.eventDate && <div style={{ color: "red" }}>{fieldErrors.eventDate}</div>}
+      <FormLabel mb={1}>Event Date</FormLabel>
+      <Input type="date" value={eventDate} onChange={(e) => setEventDate(e.target.value)} />
+    </FormControl>
+    <FormControl>
+      {fieldErrors.eventLocation && <div style={{ color: "red" }}>{fieldErrors.eventLocation}</div>}
+      <FormLabel mb={1}>Event Location</FormLabel>
+      <Input value={eventLocation} onChange={(e) => setEventLocation(e.target.value)} />
+    </FormControl>
+    <FormControl>
+      {fieldErrors.numTopTables && <div style={{ color: "red" }}>{fieldErrors.numTopTables}</div>}
+      <FormLabel mb={1}>Number of Top Tables</FormLabel>
+      <Input type="number" value={numTopTables} onChange={(e) => setNumTopTables(parseInt(e.target.value))} />
+    </FormControl>
+    <FormControl>
+      {fieldErrors.maxSizeTopTable && <div style={{ color: "red" }}>{fieldErrors.maxSizeTopTable}</div>}
+      <FormLabel mb={1}>Max Size of Top Table</FormLabel>
+      <Input type="number" value={maxSizeTopTable} onChange={(e) => setMaxSizeTopTable(parseInt(e.target.value))} />
+    </FormControl>
+    <FormControl>
+      {fieldErrors.numNormalTables && <div style={{ color: "red" }}>{fieldErrors.numNormalTables}</div>}
+      <FormLabel mb={1}>Number of Normal Tables</FormLabel>
+      <Input type="number" value={numNormalTables} onChange={(e) => setNumNormalTables(parseInt(e.target.value))} />
+    </FormControl>
+    <FormControl>
+      {fieldErrors.maxSizeNormalTable && <div style={{ color: "red" }}>{fieldErrors.maxSizeNormalTable}</div>}
+      <FormLabel mb={1}>Max Size of Normal Table</FormLabel>
+      <Input type="number" value={maxSizeNormalTable} onChange={(e) => setMaxSizeNormalTable(parseInt(e.target.value))} />
+    </FormControl>
+  </Stack>
+);
+
 
   return (
     <>
@@ -86,7 +135,7 @@ export const CreateNewEventButton: React.FC<CreateNewEventButtonProps> = ({ hand
         isOpen={isOpenNew}
         onClose={() => setIsOpenNew(false)}
         title="Create New Event"
-        handleSubmit={handleSubmit} // Use the new handleSubmit function
+        handleSubmit={handleSubmit}
       >
         {newEventForm}
       </ReusableModal>
