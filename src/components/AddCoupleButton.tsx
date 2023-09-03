249ee5d4cf3908be3329c2a6c1195d63cf562bc5
Andrew Maddox <andrew.iain.maddox@gmail.com>
2023-09-03 11:06:09 +0100

    Styling for Add Couple element

--- src/components/AddCoupleButton.tsx
+++ src/components/AddCoupleButton.tsx
@@ -1,16 +1,28 @@
 import React, { useState } from "react";
-import { Button, FormControl, FormLabel, Input, Checkbox } from "@chakra-ui/react";
+import {
+  Button,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  HStack,
+  Flex,
+  Box,
+} from "@chakra-ui/react";
 import { ReusableModal } from "./ReusableModal";
 import WriteData from "./WriteData";
 import { useEvent } from "./EventContext";
 import { set, z } from "zod";
 
 interface AddCoupleButtonProps {
-    setShouldRefetch: React.Dispatch<React.SetStateAction<boolean>>; // Add this line
-  }
+  setShouldRefetch: React.Dispatch<React.SetStateAction<boolean>>; // Add this line
+}
 
-  export const AddCoupleButton: React.FC<AddCoupleButtonProps> = ({ setShouldRefetch }) => {
-    const { eventData } = useEvent();
+export const AddCoupleButton: React.FC<AddCoupleButtonProps> = ({
+  setShouldRefetch,
+}) => {
+  const { eventData } = useEvent();
   const eventId = eventData.event_id;
 
   const [isOpen, setIsOpen] = useState(false);
@@ -23,32 +35,41 @@ interface AddCoupleButtonProps {
   const [shouldWriteData, setShouldWriteData] = useState(false);
   const [fieldErrors, setFieldErrors] = useState<{ [key: string]: string }>({});
 
-  const newCoupleSchema = z.object({
-    nameOne: z.string().min(3, "Name One is required"),
-    isGroomOne: z.boolean(),
-    isBrideOne: z.boolean(),
-    nameTwo: z.string().min(3, "Name Two is required"),
-    isGroomTwo: z.boolean(),
-    isBrideTwo: z.boolean(),
-  })
-  .refine(data => {
-    return (data.isGroomOne && !data.isBrideOne) || (!data.isGroomOne && data.isBrideOne);
-  }, {
-    message: "Please select a status for person one",
-    path: ['isGroomOne', 'isBrideOne']
-  })
-  .refine(data => {
-    return (data.isGroomTwo && !data.isBrideTwo) || (!data.isGroomTwo && data.isBrideTwo);
-  }, {
-    message: "Please select a status for person two",
-    path: ['isGroomTwo', 'isBrideTwo']
-  });
-  
-  
-  
-  
+  const newCoupleSchema = z
+    .object({
+      nameOne: z.string().min(3, "Name One is required"),
+      isGroomOne: z.boolean(),
+      isBrideOne: z.boolean(),
+      nameTwo: z.string().min(3, "Name Two is required"),
+      isGroomTwo: z.boolean(),
+      isBrideTwo: z.boolean(),
+    })
+    .refine(
+      (data) => {
+        return (
+          (data.isGroomOne && !data.isBrideOne) ||
+          (!data.isGroomOne && data.isBrideOne)
+        );
+      },
+      {
+        message: "Please select a status for person one",
+        path: ["isGroomOne", "isBrideOne"],
+      }
+    )
+    .refine(
+      (data) => {
+        return (
+          (data.isGroomTwo && !data.isBrideTwo) ||
+          (!data.isGroomTwo && data.isBrideTwo)
+        );
+      },
+      {
+        message: "Please select a status for person two",
+        path: ["isGroomTwo", "isBrideTwo"],
+      }
+    );
 
-const handleCoupleSubmit = () => {
+  const handleCoupleSubmit = () => {
     const parsedData = newCoupleSchema.safeParse({
       nameOne,
       isGroomOne,
@@ -57,7 +78,7 @@ const handleCoupleSubmit = () => {
       isGroomTwo,
       isBrideTwo,
     });
-  
+
     if (!parsedData.success) {
       const errors: { [key: string]: string } = {};
       parsedData.error.issues.forEach((issue) => {
@@ -66,12 +87,12 @@ const handleCoupleSubmit = () => {
       setFieldErrors(errors);
       return;
     }
-  
+
     if (eventId === undefined) {
       console.error("Event ID is undefined. Aborting submit.");
       return;
     }
-  
+
     setShouldWriteData(true);
     setIsOpen(false);
   };
@@ -92,45 +113,97 @@ const handleCoupleSubmit = () => {
     setShouldWriteData(false);
   };
 
-
   const form = (
     <>
-    <FormControl>
-      {fieldErrors.nameOne && <div style={{ color: "red" }}>{fieldErrors.nameOne}</div>}
-      <FormLabel>Name One</FormLabel>
-      <Input value={nameOne} onChange={(e) => setNameOne(e.target.value)} />
-    </FormControl>
-    <FormControl>
-      {fieldErrors.isGroomOne && <div style={{ color: "red" }}>{fieldErrors.isGroomOne}</div>}
-      <Checkbox isChecked={isGroomOne} onChange={(e) => setIsGroomOne(e.target.checked)}>
-        Is Groom
-      </Checkbox>
-    </FormControl>
-    <FormControl>
-      {fieldErrors.isBrideOne && <div style={{ color: "red" }}>{fieldErrors.isBrideOne}</div>}
-      <Checkbox isChecked={isBrideOne} onChange={(e) => setIsBrideOne(e.target.checked)}>
-        Is Bride
-      </Checkbox>
-    </FormControl>
-    <FormControl>
-      {fieldErrors.nameTwo && <div style={{ color: "red" }}>{fieldErrors.nameTwo}</div>}
-      <FormLabel>Name Two</FormLabel>
-      <Input value={nameTwo} onChange={(e) => setNameTwo(e.target.value)} />
-    </FormControl>
-    <FormControl>
-      {fieldErrors.isGroomTwo && <div style={{ color: "red" }}>{fieldErrors.isGroomTwo}</div>}
-      <Checkbox isChecked={isGroomTwo} onChange={(e) => setIsGroomTwo(e.target.checked)}>
-        Is Groom
-      </Checkbox>
-    </FormControl>
-    <FormControl>
-      {fieldErrors.isBrideTwo && <div style={{ color: "red" }}>{fieldErrors.isBrideTwo}</div>}
-      <Checkbox isChecked={isBrideTwo} onChange={(e) => setIsBrideTwo(e.target.checked)}>
-        Is Bride
-      </Checkbox>
-    </FormControl>
-  </>
-  
+      <Stack spacing={6}>
+        <FormControl>
+          <Box
+            width="100%"
+            color="red"
+            height={fieldErrors.nameOne ? "auto" : "0"}
+          >
+            {fieldErrors.nameOne}
+          </Box>
+          <FormLabel>First Person</FormLabel>
+          <Input value={nameOne} onChange={(e) => setNameOne(e.target.value)} />
+        </FormControl>
+        <Box
+          width="100%"
+          height={
+            fieldErrors.isGroomOne || fieldErrors.isBrideOne ? "auto" : "0"
+          }
+          mb={fieldErrors.isGroomOne || fieldErrors.isBrideOne ? 4 : 0} // Conditional bottom margin
+        >
+          {fieldErrors.isGroomOne && (
+            <Box color="red">{fieldErrors.isGroomOne}</Box>
+          )}
+          {fieldErrors.isBrideOne && (
+            <Box color="red">{fieldErrors.isBrideOne}</Box>
+          )}
+        </Box>
+        <HStack mt={-8}>
+          <Flex direction="column" align="start">
+            <Checkbox
+              isChecked={isGroomOne}
+              onChange={(e) => setIsGroomOne(e.target.checked)}
+            >
+              Groom
+            </Checkbox>
+          </Flex>
+          <Flex direction="column" align="start">
+            <Checkbox
+              isChecked={isBrideOne}
+              onChange={(e) => setIsBrideOne(e.target.checked)}
+            >
+              Bride
+            </Checkbox>
+          </Flex>
+        </HStack>
+        <FormControl>
+          <Box
+            width="100%"
+            color="red"
+            height={fieldErrors.nameTwo ? "auto" : "0"}
+          >
+            {fieldErrors.nameTwo}
+          </Box>
+          <FormLabel>Second Person</FormLabel>
+          <Input value={nameTwo} onChange={(e) => setNameTwo(e.target.value)} />
+        </FormControl>
+        <Box
+          width="100%"
+          height={
+            fieldErrors.isGroomTwo || fieldErrors.isBrideTwo ? "auto" : "0"
+          }
+          mb={fieldErrors.isGroomOne || fieldErrors.isBrideOne ? 4 : 0} // Conditional bottom margin
+        >
+          {fieldErrors.isGroomTwo && (
+            <Box color="red">{fieldErrors.isGroomTwo}</Box>
+          )}
+          {fieldErrors.isBrideTwo && (
+            <Box color="red">{fieldErrors.isBrideTwo}</Box>
+          )}
+        </Box>
+        <HStack mt={-8}>
+          <Flex direction="column" align="start">
+            <Checkbox
+              isChecked={isGroomTwo}
+              onChange={(e) => setIsGroomTwo(e.target.checked)}
+            >
+              Groom
+            </Checkbox>
+          </Flex>
+          <Flex direction="column" align="start">
+            <Checkbox
+              isChecked={isBrideTwo}
+              onChange={(e) => setIsBrideTwo(e.target.checked)}
+            >
+              Bride
+            </Checkbox>
+          </Flex>
+        </HStack>
+      </Stack>
+    </>
   );
 
   return (
@@ -139,7 +212,7 @@ const handleCoupleSubmit = () => {
       <ReusableModal
         isOpen={isOpen}
         onClose={() => setIsOpen(false)}
-        title="New Couple"
+        title="Add Couple"
         handleSubmit={handleCoupleSubmit}
       >
         {form}
@@ -169,7 +242,8 @@ const handleCoupleSubmit = () => {
             onFailure={onFailure}
           />
         </>
-      )};
+      )}
+      ;
     </>
-    );
-}
+  );
+};
