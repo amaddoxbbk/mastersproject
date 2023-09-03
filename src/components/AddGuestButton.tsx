e633b28aaf7b50f1cc88f44d61c6d8af92c65f65
Andrew Maddox <andrew.iain.maddox@gmail.com>
2023-09-03 15:39:08 +0100

    Clear form on submit

--- src/components/AddGuestButton.tsx
+++ src/components/AddGuestButton.tsx
@@ -5,13 +5,14 @@ import {
   FormLabel,
   Input,
   Checkbox,
+  Stack,
+  HStack,
 } from "@chakra-ui/react";
 import { ReusableModal } from "./ReusableModal";
 import WriteData from "./WriteData";
 import { useEvent } from "./EventContext";
 import GenericDropdown from "./GenericDropdown";
 import { set, z } from "zod"; // Import Zod
-
 
 const relationshipOptions = [
   { value: "family_of_bride", label: "Family of Bride" },
@@ -24,14 +25,22 @@ const relationshipOptions = [
 const newGuestSchema = z.object({
   name: z.string().min(3, "Guest name is required"),
   plusOne: z
-  .string()
-  .optional()
-  .refine((value) => value === undefined || value === "" || value.length >= 3, {
-    message: "Plus One name must be at least 3 characters long",
-  }),
+    .string()
+    .optional()
+    .refine(
+      (value) => value === undefined || value === "" || value.length >= 3,
+      {
+        message: "Plus One name must be at least 3 characters long",
+      }
+    ),
 
-    relationship: z.enum(["family_of_bride", "friends_of_bride", "family_of_groom", "friends_of_groom"]),
-    specialStatus: z.string().optional(),
+  relationship: z.enum([
+    "family_of_bride",
+    "friends_of_bride",
+    "family_of_groom",
+    "friends_of_groom",
+  ]),
+  specialStatus: z.string().optional(),
 });
 
 interface AddGuestButtonProps {
@@ -41,7 +50,7 @@ interface AddGuestButtonProps {
 
 export const AddGuestButton: React.FC<AddGuestButtonProps> = ({
   guests,
-  setShouldRefetch
+  setShouldRefetch,
 }) => {
   const { eventData } = useEvent();
   const eventId = eventData.event_id;
@@ -60,8 +69,8 @@ export const AddGuestButton: React.FC<AddGuestButtonProps> = ({
   const [selectedBlacklist, setSelectedBlacklist] = useState<string[]>([]);
   const [fieldErrors, setFieldErrors] = useState<{ [key: string]: string }>({});
   const [isBride, setIsBride] = useState(false);
-const [isGroom, setIsGroom] = useState(false);
-
+  const [isGroom, setIsGroom] = useState(false);
+  const [showPlusOne, setShowPlusOne] = useState(false);
 
   const handleBlacklistChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const selectedOptions = Array.from(e.target.selectedOptions).map(
@@ -96,8 +105,11 @@ const [isGroom, setIsGroom] = useState(false);
     if (!parsedData.success) {
       const errors: { [key: string]: string } = {};
       parsedData.error.issues.forEach((issue) => {
-        if (issue.path[0] === 'relationship' && issue.code === 'invalid_enum_value') {
-          errors[issue.path[0]] = 'Please select a relationship';
+        if (
+          issue.path[0] === "relationship" &&
+          issue.code === "invalid_enum_value"
+        ) {
+          errors[issue.path[0]] = "Please select a relationship";
         } else {
           errors[issue.path[0]] = issue.message;
         }
@@ -123,6 +135,8 @@ const [isGroom, setIsGroom] = useState(false);
     setSelectedBlacklist([]); // Reset blacklist
     setShouldWriteData(false);
     setShouldRefetch(true);
+    setShowPlusOne(false);
+    setFieldErrors({}); // Reset all field errors
   };
 
   const onFailure = (error: any) => {
@@ -136,62 +150,89 @@ const [isGroom, setIsGroom] = useState(false);
 
   const form = (
     <>
-      <FormControl>
-        {fieldErrors.name && (
-          <div style={{ color: "red" }}>{fieldErrors.name}</div>
-        )}
-        <FormLabel>Guest Name</FormLabel>
-        <Input value={name} onChange={(e) => setName(e.target.value)} />
-      </FormControl>
-      <FormControl>
-        {fieldErrors.plusOne && (
-          <div style={{ color: "red" }}>{fieldErrors.plusOne}</div>
-        )}
-        <FormLabel>Plus One</FormLabel>
-        <Input value={plusOne} onChange={(e) => setPlusOne(e.target.value)} />
-      </FormControl>
-      <FormControl>
-      {fieldErrors.relationship && <div style={{ color: "red" }}>{fieldErrors.relationship}</div>}
-        <FormLabel>Relationship</FormLabel>
-        <GenericDropdown
-          onSelect={(value) => setRelationship(value)}
-          selectedValue={relationship}
-          options={relationshipOptions}
-          title="Relationship"
-        />
-      </FormControl>
-      <FormControl>
-        <FormLabel>Blacklist Attendee IDs</FormLabel>
-        {guests && guests.length > 0 ? (
-          <>
-            <GenericDropdown
-              onSelect={handleSelect}
-              selectedValue={selectedBlacklist}
-              options={options}
-              title="Blacklist"
-            />
-            {selectedBlacklist.length > 0 && (
-              <div>
-                <h3>Blacklisted Guests:</h3>
-                <ul>
-                  {selectedBlacklist.map((id, index) => (
-                    <li key={index}>{getBlacklistNameById(id)}</li>
-                  ))}
-                </ul>
-              </div>
+      <Stack spacing={6}>
+        <FormControl>
+          {fieldErrors.name && (
+            <div style={{ color: "red" }}>{fieldErrors.name}</div>
+          )}
+          <FormLabel>Guest Name</FormLabel>
+          <Input value={name} onChange={(e) => setName(e.target.value)} />
+        </FormControl>
+
+        <HStack spacing={2}>
+          <FormLabel>Plus One:</FormLabel>
+          <Checkbox
+            isChecked={showPlusOne}
+            onChange={() => setShowPlusOne(true)}
+          >
+            Yes
+          </Checkbox>
+          <Checkbox
+            isChecked={!showPlusOne}
+            onChange={() => setShowPlusOne(false)}
+          >
+            No
+          </Checkbox>
+        </HStack>
+
+        {showPlusOne && (
+          <FormControl mt={-2}>
+            {fieldErrors.plusOne && (
+              <div style={{ color: "red" }}>{fieldErrors.plusOne}</div>
             )}
-          </>
-        ) : (
-          <p>No guests available for blacklist.</p>
+            <FormLabel>Plus One</FormLabel>
+            <Input
+              value={plusOne}
+              onChange={(e) => setPlusOne(e.target.value)}
+            />
+          </FormControl>
         )}
-      </FormControl>
-      <FormControl>
-        <FormLabel>Special Status</FormLabel>
-        <Input
-          value={specialStatus}
-          onChange={(e) => setSpecialStatus(e.target.value)}
-        />
-      </FormControl>
+
+        <FormControl>
+          {fieldErrors.relationship && (
+            <div style={{ color: "red" }}>{fieldErrors.relationship}</div>
+          )}
+          <FormLabel>Relationship</FormLabel>
+          <GenericDropdown
+            onSelect={(value) => setRelationship(value)}
+            selectedValue={relationship}
+            options={relationshipOptions}
+            title="Relationship"
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Blacklist Attendee IDs</FormLabel>
+          {guests && guests.length > 0 ? (
+            <>
+              <GenericDropdown
+                onSelect={handleSelect}
+                selectedValue={selectedBlacklist}
+                options={options}
+                title="Blacklist"
+              />
+              {selectedBlacklist.length > 0 && (
+                <div>
+                  <h3>Blacklisted Guests:</h3>
+                  <ul>
+                    {selectedBlacklist.map((id, index) => (
+                      <li key={index}>{getBlacklistNameById(id)}</li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </>
+          ) : (
+            <p>No guests available for blacklist.</p>
+          )}
+        </FormControl>
+        <FormControl>
+          <FormLabel>Special Status</FormLabel>
+          <Input
+            value={specialStatus}
+            onChange={(e) => setSpecialStatus(e.target.value)}
+          />
+        </FormControl>
+      </Stack>
     </>
   );
 
@@ -201,7 +242,7 @@ const [isGroom, setIsGroom] = useState(false);
       <ReusableModal
         isOpen={isOpen}
         onClose={() => setIsOpen(false)}
-        title="New Guest"
+        title="Add New Guest"
         handleSubmit={handleGuestSubmit}
       >
         {form}
