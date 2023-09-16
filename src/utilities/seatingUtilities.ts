54d058a3649bdbced4dcc97fdd245fba6139d74c
Andrew Maddox <andrew.iain.maddox@gmail.com>
2023-09-16 18:53:51 +0100

    Random tables now include plus ones

--- src/utilities/seatingUtilities.ts
+++ src/utilities/seatingUtilities.ts
@@ -5,25 +5,67 @@ export interface TableData {
     names: string[];
   }
   
-  // Generates a random arrangement of tables based on a list of guests and table sizes
   export const generateRandomTable = (
     guests: any[],
     sizeNormalTablesNumber: number
   ): TableData[] => {
+    console.log("Original guest list:", guests);
+  
     let tables: TableData[] = [];
     let currentTable: string[] = [];
     let currentTableIndex = 1;
   
-    // Shuffle the guests to randomize the seating
-    const shuffledGuests = [...guests].sort(() => Math.random() - 0.5);
+    // Group guests with their partners
+    const groupedGuests: any[] = [];
+    const singleGuests: any[] = [];
   
-    for (const guest of shuffledGuests) {
-      currentTable.push(guest.attendee_name);
+    for (const guest of guests) {
+      if (guest.partner_to !== null) {
+        const mainGuest = guests.find(g => g.attendee_id === guest.partner_to);
+        if (mainGuest) {
+          groupedGuests.push([mainGuest, guest]);
+        }
+      } else if (!guests.some(g => g.partner_to === guest.attendee_id)) {
+        singleGuests.push(guest);
+      }
+    }
+  
+    console.log("Grouped guests:", groupedGuests);
+    console.log("Single guests:", singleGuests);
+  
+    // Shuffle the guests to randomize the seating
+    const shuffledGuests = [...groupedGuests, ...singleGuests].sort(() => Math.random() - 0.5);
+  
+    console.log("Shuffled guests:", shuffledGuests);
+  
+    for (const guestOrGroup of shuffledGuests) {
+      if (Array.isArray(guestOrGroup)) {
+        // It's a group
+        if (currentTable.length <= sizeNormalTablesNumber - 2) {
+          currentTable.push(guestOrGroup[0].attendee_name, guestOrGroup[1].attendee_name);
+          console.log(`Adding ${guestOrGroup[0].attendee_name} and ${guestOrGroup[1].attendee_name} to the current table.`);
+        } else {
+          tables.push({
+            title: `Table ${currentTableIndex}`,
+            names: [...currentTable]
+          });
+          console.log(`Closing Table ${currentTableIndex} with names: ${currentTable}`);
+          currentTable = [guestOrGroup[0].attendee_name, guestOrGroup[1].attendee_name];
+          console.log(`Starting a new table with ${guestOrGroup[0].attendee_name} and ${guestOrGroup[1].attendee_name}`);
+          currentTableIndex++;
+        }
+      } else {
+        // It's a single
+        currentTable.push(guestOrGroup.attendee_name);
+        console.log(`Adding ${guestOrGroup.attendee_name} to the current table.`);
+      }
+  
       if (currentTable.length === sizeNormalTablesNumber) {
         tables.push({
           title: `Table ${currentTableIndex}`,
           names: [...currentTable]
         });
+        console.log(`Closing Table ${currentTableIndex} with names: ${currentTable}`);
         currentTable = [];
         currentTableIndex++;
       }
@@ -35,24 +77,10 @@ export interface TableData {
         title: `Table ${currentTableIndex}`,
         names: [...currentTable]
       });
+      console.log(`Closing Table ${currentTableIndex} with remaining names: ${currentTable}`);
     }
   
+    console.log("Final tables:", tables);
     return tables;
-  }
-  
-  // Checks if a table arrangement is valid according to the given rules
-  export const isValidArrangement = (
-    tables: TableData[],
-    sizeNormalTablesNumber: number
-  ): boolean => {
-    // Check if any table exceeds the maximum size
-    for (const table of tables) {
-      if (table.names.length > sizeNormalTablesNumber) {
-        return false;
-      }
-    }
-    return true;
   };
-  
-  // Add more utility functions as needed
   
